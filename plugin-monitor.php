44ff4c770928c0f9e00822351f58b04dbbfd3ccf
Ryan Williams <rwilliams@mla.org>
2018-06-19 12:36:04 -0400

    first

---
+++ plugin-monitor.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Plugin Monitor
+ * Plugin URI:      https://github.com/mlaa/plugin-monitor.git
+ * Description:     Plugin Monitor
+ * Author:          MLA
+ * Author URI:      https://github.com/mlaa
+ * Text Domain:     plugin-monitor
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Plugin_Monitor
+ */
+
+// Your code starts here.
